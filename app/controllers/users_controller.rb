25b9bda8a3b151c92195fa152178d0dbc63bcb27
darryl <darrylmangibin@gmail.com>
2020-02-19 10:26:31 +0800

    add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     end
 
     def show 
-        
+        @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
     end
 
     private
