8f2258c14d9e889254c84f788ad2ebc3f3b45c4b
Darryl Mangibin <darrylmangibin@gmail.com>
2020-03-04 23:07:15 +0800

    practicing git

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,23 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
+  def new; end
 
-    def new 
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully login'
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'There was something wrong with your login information'
+      render 'new'
     end
+  end
 
-    def create
-        user = User.find_by(email: params[:session][:email].downcase)
-        if user && user.authenticate(params[:session][:password])
-            session[:user_id] = user.id
-            flash[:success] = "You have successfully login"
-            redirect_to user_path(user)
-        else 
-            flash.now[:danger] = "There was something wrong with your login information"
-            render 'new'
-        end
-    end 
-
-    def destroy 
-        session[:user_id] = nil
-        flash[:success] = "Successfully log out"
-        redirect_to root_path
-    end
-
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Successfully log out'
+    redirect_to root_path
+  end
 end
