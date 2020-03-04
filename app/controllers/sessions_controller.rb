34590b4eb85ec10fdd3b68bc3af97699330e7c22
Darryl Mangibin <darrylmangibin@gmail.com>
2020-03-04 23:08:42 +0800

    practicing git

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  def new
-    @posts = Post.all
-    end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
